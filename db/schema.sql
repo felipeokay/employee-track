fe8044705b9a4608bbe64433e4b89d09d799c9d8
unknown <kevinserrano0823@gmail.com>
2023-10-24 18:54:40 -0500

    created tables inside database

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,26 @@
-CREATE DATABASE employee_db
-
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 USE employee_db;
 
-CREATE TABLE department (
-
+CREATE TABLE departmentS (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+department_name VARCHAR(40)
 );
 
 CREATE TABLE role (
-
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(40),
+salary DECIMAL(10,2),
+department_id INT,
+FOREIGN KEY (department_id)
+REFERENCES departments(id)
+ON DELETE SET NULL
 );
 
 CREATE TABLE emplopyee (
-
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT NOT NULL,
+manager_id INT NOT NULL
 );
